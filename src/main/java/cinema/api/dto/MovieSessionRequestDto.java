19cca5db5ca1e4dc830d7015b8590adc6600fcc3
Cristian Amorim <camorim1100@gmail.com>
2026-08-04 10:30:21 -0300

    Atualizando a classe de MovieSessionRequestDTO

---
+++ src/main/java/cinema/api/dto/MovieSessionRequestDto.java
@@ -0,0 +1,22 @@
+package cinema.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MovieSessionRequestDto {
+
+    @JsonProperty(required = true)
+    private String room;
+    @JsonProperty(required = true)
+    private LocalDateTime sessionTime;
+
+}
