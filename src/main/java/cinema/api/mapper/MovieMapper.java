362ba931dd5ca6991ba2ed292bf22d136cec2d93
Cristian Amorim <camorim1100@gmail.com>
2026-08-04 10:53:44 -0300

    Finalizando a classe MovieMapper

--- src/main/java/cinema/api/mapper/MovieMapper.java
+++ src/main/java/cinema/api/mapper/MovieMapper.java
@@ -1,4 +1,22 @@
 package cinema.api.mapper;
 
+import cinema.api.dto.MovieRequestDto;
+import cinema.api.dto.MovieResponseDto;
+import cinema.api.model.Movie;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface MovieMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "durationMinutes", ignore = true)
+    Movie toMovie(MovieRequestDto dto);
+
+    MovieResponseDto toResponeDto(Movie movie);
+
+    List<MovieResponseDto> toListResponseDto(List<Movie> movies);
+
 }
