8a46aa78df7eb07f42f926556db1f961986e3738
Cristian Amorim <camorim1100@gmail.com>
2026-08-04 10:38:52 -0300

    Criando a classe de MovieSessionResponseDto

---
+++ src/main/java/cinema/api/dto/MoviSessionResponseDto.java
@@ -0,0 +1,27 @@
+package cinema.api.dto;
+
+import cinema.api.model.Movie;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MoviSessionResponseDto {
+
+    @JsonProperty(required = true)
+    private String room;
+    @JsonProperty(required = true)
+    private LocalDateTime sessionTime;
+
+    @ManyToOne
+    @JsonProperty(required = true)
+    private Movie movie;
+}
