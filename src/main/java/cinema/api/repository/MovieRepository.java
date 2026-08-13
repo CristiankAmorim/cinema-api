7d00292ae777aa6f772eba8eefabc3eb9fa13cf5
Cristian Amorim <camorim1100@gmail.com>
2026-08-13 11:21:22 -0300

    Criando um novo método para verificar se um filme já existe pelo título

--- src/main/java/cinema/api/repository/MovieRepository.java
+++ src/main/java/cinema/api/repository/MovieRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     Movie findByTitle(String title);
+
+    boolean existsByTitleIgnoreCase(String title);
 }
